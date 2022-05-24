e5bbf96feae4ab4444666b5c4fee2816b394c2d9
NsikanPatrick <nsikanpatrick69@gmail.com>
2022-05-24 05:56:33 +0200

    commit for demo site

--- my_portfolio_website/settings.py
+++ my_portfolio_website/settings.py
@@ -1,4 +1,7 @@
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 """
 Django settings for my_portfolio_website project.
 
@@ -126,7 +129,11 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_FILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
